a96ce821d92c837b162ecad881c942158e9f2468
Gabriel Ribeiro Noronha <gabrielrib98@gmail.com>
2024-03-21 16:54:22 -0300

    DatePickerAdded

---
+++ FoodsClassificator/FoodsClassificator/Utilities/Extensions/DateFormatterExtension.swift
@@ -0,0 +1,8 @@
+//
+//  DateFormatterExtension.swift
+//  FoodsClassificator
+//
+//  Created by Gabriel Ribeiro Noronha on 21/03/24.
+//
+
+import Foundation
---
+++ FoodsClassificator/FoodsClassificator/Views/Home/Components/DatePickerComponentHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  DatePickerComponentHomeView.swift
+//  FoodsClassificator
+//
+//  Created by Gabriel Ribeiro Noronha on 21/03/24.
+//
+
+import SwiftUI
+
+struct DatePickerComponentHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DatePickerComponentHomeView()
+}
