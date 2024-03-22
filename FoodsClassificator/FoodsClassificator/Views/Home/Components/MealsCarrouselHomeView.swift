3558e54d73c88d196cecf5b1dd647333ca9c50f9
Gabriel Ribeiro Noronha <gabrielrib98@gmail.com>
2024-03-22 14:35:04 -0300

    Carrousel de alimentos funcionando

---
+++ FoodsClassificator/FoodsClassificator/Views/Home/Components/MealsCarrouselHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  MealsCarrouselHomeView.swift
+//  FoodsClassificator
+//
+//  Created by Gabriel Ribeiro Noronha on 21/03/24.
+//
+
+import SwiftUI
+
+struct MealsCarrouselHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealsCarrouselHomeView()
+}
