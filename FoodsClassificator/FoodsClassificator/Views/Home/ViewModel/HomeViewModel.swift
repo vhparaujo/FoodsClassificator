3b8553a8216cecdd2d9a4c8270a8b80e6b219146
Gabriel Ribeiro Noronha <68724145+bielr98@users.noreply.github.com>
2024-03-22 15:19:33 -0300

    Revert "Home view"

--- FoodsClassificator/FoodsClassificator/Views/Home/ViewModel/HomeViewModel.swift
+++ FoodsClassificator/FoodsClassificator/Views/Home/ViewModel/HomeViewModel.swift
@@ -9,26 +9,10 @@ import SwiftUI
 
 @Observable class HomeViewModel {
     
-    var userName: String = "Olá, Fulano!"
+    
+    var userName: String = "Olá, Fulano"
     var userPhoto: String = "labelImage"
     var macrosValue: Int = 100
     var streakNumber: Int = 20
-    var calories: Int = 2896
-    
-    var progressBarValue: Double = 0.7
-    
-    var maxProgressBarValue: Double = 0.88
-    var initialProgressBarValue: Double = 0.12
-    
-    init() {
-        maxProgressBarValueFunc()
-    }
-    
-    func maxProgressBarValueFunc() {
-        if self.progressBarValue > maxProgressBarValue {
-            self.progressBarValue = initialProgressBarValue
-        }
-    }
 
-    
 }
