875278078fcea3601f0775e0cad7b0af34ace9d2
Gabriel Ribeiro Noronha <gabrielrib98@gmail.com>
2024-03-25 14:09:35 -0300

    Pagina inicial alterada

--- FoodsClassificator/FoodsClassificator/App/FoodsClassificatorApp.swift
+++ FoodsClassificator/FoodsClassificator/App/FoodsClassificatorApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FoodsClassificatorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView().preferredColorScheme(.light)
+            HomeView().preferredColorScheme(.light)
         }
     }
 }
